de878ec6a1f1e49951859fe395d7d193ce5bbc85
Luke Meyer <lmeyer@redhat.com>
2023-10-10 15:10:41 -0400

    rename art-common/artcommonlib -> artcommon

--- doozer/doozerlib/cli/scan_sources.py
+++ doozer/doozerlib/cli/scan_sources.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import click
 import yaml
 
-from artcommonlib.rhcos import get_primary_container_name
+from artcommon.rhcos import get_primary_container_name
 from doozerlib import brew, exectools, rhcos, util
 from doozerlib.cli import cli, pass_runtime
 from doozerlib.cli import release_gen_payload as rgp
